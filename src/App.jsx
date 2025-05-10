5ed34fe6d08ca5a9a3f818a2492bb0cc5807820e
Prince Biile <biileprinceyennuyar5@gmail.com>
2025-05-10 23:21:04 +0000

    corrected an import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import Navbar from "./components/NavBar/NavBar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
+import Navbar from "./components/Navbar/Navbar";
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
   return (
